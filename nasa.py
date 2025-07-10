cd2dd0b0c750e5cb5df4d952bd88e6f09f45b1e9
Voedoe-H <heermannhagen@gmail.com>
2025-07-10 14:09:15 +0200

    cycle sorting update

--- nasa.py
+++ nasa.py
@@ -1,20 +1,34 @@
 import pandas as pd
 import os
 
+
+def parse_start_time(time_str):
+    try:
+        time_floats = [float(i) for i in time_str.strip("[]").replace("e+03", "000").split()]
+        return pd.to_datetime(time_floats[:6], errors='coerce')  # year, month, day, hour, minute, second
+    except:
+        return pd.NaT
+
+
 data_dir = "./NASABat/cleaned_dataset/data/"
 
 battery_data = {}
 
 df_meta = pd.read_csv("./NASABat/cleaned_dataset/metadata.csv")
-
+df_meta["parsed_time"] = df_meta["start_time"].apply(parse_start_time)
 unique_types = df_meta["type"].unique()
 print("Unique types:", unique_types)
 
 #print(df_meta.head)
 #print(df_meta["type"].head)
 #print(df_meta["battery_id"].head)
+# Impedance File Structure: Sense_current,Battery_current,Current_ratio,Battery_impedance,Rectified_Impedance
+# Discharge File Structure: Voltage_measured,Current_measured,Temperature_measured,Current_load,Voltage_load,Time
+# Charge File Structure: Voltage_measured,Current_measured,Temperature_measured,Current_charge,Voltage_charge,Time
 
 grouped = df_meta.groupby("battery_id")
+unique_ids = df_meta["battery_id"].unique()
+print(f"Number of batteries:{len(grouped)}")
 
 for battery, group in grouped:
     print(f"\nBattery: {battery}")
